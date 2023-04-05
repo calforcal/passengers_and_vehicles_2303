c106ce92d27ee29048c071fa063116f6d95c1380
Michael Callahan <mcalla242@gmail.com>
2023-04-05 09:21:19 -0600

    Add driver? method

--- lib/passenger.rb
+++ lib/passenger.rb
@@ -4,9 +4,14 @@ class Passenger
   def initialize(details)
     @name = details["name"]
     @age = details["age"]
+    @driver = false
   end
 
   def adult?
     if @age >= 18 then return true else return false end
+  end
+
+  def driver?
+    @driver
   end
 end
