3c5d2af9f9263be199c9d699bdb6d6dc726451aa
Michael Callahan <mcalla242@gmail.com>
2023-04-05 09:20:18 -0600

    Add adult? method

--- lib/passenger.rb
+++ lib/passenger.rb
@@ -2,7 +2,11 @@ class Passenger
   attr_reader :name, :age
 
   def initialize(details)
-    @name = details[:name]
-    @age = details[:age]
+    @name = details["name"]
+    @age = details["age"]
+  end
+
+  def adult?
+    if @age >= 18 then return true else return false end
   end
 end
