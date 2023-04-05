7aeaa17ced4a49b4a879e14a991e6cd2894fd5be
Michael Callahan <mcalla242@gmail.com>
2023-04-05 09:49:52 -0600

    Add initial test setup and initialize testing specs

---
+++ spec/park_spec.rb
@@ -0,0 +1,24 @@
+require "./lib/passenger"
+require "./lib/vehicle"
+require "./lib/park"
+
+RSpec.describe Park do
+  before(:each) do
+    @water_world = Park.new("Water World", 20)
+    @vehicle = Vehicle.new("2001", "Honda", "Civic")
+    @charlie = Passenger.new({"name" => "Charlie", "age" => 18})  
+    @jude = Passenger.new({"name" => "Jude", "age" => 20})
+    @taylor = Passenger.new({"name" => "Taylor", "age" => 12}) 
+  end
+
+  describe "#initialize" do
+    it "initializes" do
+      expect(@water_world).to be_an_instance_of(Park)
+    end
+
+    it "initializes with attributes" do
+      expect(@water_world.name).to eq("Water World")
+      expect(@water_world.admission_price).to eq(20)
+    end
+  end
+end
